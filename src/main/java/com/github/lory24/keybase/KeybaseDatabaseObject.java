22caa074f9c39b82d50423f1c09dcb0d17928137
Lorenzo Rocca <51451299+LoRy24@users.noreply.github.com>
2023-02-09 21:54:43 +0100

    Add new Javadoc

--- src/main/java/com/github/lory24/keybase/KeybaseDatabaseObject.java
+++ src/main/java/com/github/lory24/keybase/KeybaseDatabaseObject.java
@@ -4,8 +4,15 @@ import lombok.Getter;
 
 import java.util.HashMap;
 
+/**
+ * The KeybaseDatabaseObject is the object that is used during the serialization of the database file. This contains the
+ * data HashMap, that stores all the keys and values.
+ *
+ * @author                          Lorenzo Rocca
+ */
 public class KeybaseDatabaseObject {
 
+    // The Data object
     @Getter
     private final HashMap<String, Object> data = new HashMap<>();
 }
